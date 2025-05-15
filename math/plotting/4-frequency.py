11a533257a47a14f417c1139d5891777ba94c1ff
pauline12ish34 <paulineishimwe12@gmail.com>
2025-05-15 18:18:47 +0200

    4-updated the range of bins and set limit on both x&y

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -4,10 +4,16 @@ import matplotlib.pyplot as plt
 
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
-
 # your code here
-plt.hist(student_grades,bins=10,edgecolor='black')
+bins = np.arange(0, 110, 10)
+
+plt.hist(student_grades,bins=bins,edgecolor='black')
 plt.xlabel('Grades')
 plt.ylabel('Number of Students')
+plt.xlim(0,100)
+plt.ylim(0,30)
 plt.title('Project A')
 plt.show()
+
+
+
